56b4844ef2285e1715f9acc34baccf65f7997c92
Maximus Juul <Maximusjjuul@gmail.com>
2021-09-27 12:47:12 +0200

    Initial commit

---
+++ Camera_og_billedemanipulation/GlobalVars.cs
@@ -0,0 +1,11 @@
+﻿using AForge.Video.DirectShow;
+
+namespace Camera_og_billedemanipulation
+{
+
+    public class GlobalVars
+    {
+        public VideoCaptureDevice FinalVideo;
+        public FilterInfoCollection VideoCaptureDevices;
+    }
+}
