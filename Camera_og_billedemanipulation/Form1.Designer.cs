52df2a122e4e50efb157bf10a3a98e137803dc6b
Keyvan <67221764+KeyvanSK@users.noreply.github.com>
2021-11-16 09:23:42 +0100

    Merged Histogram og Timer

--- Camera_og_billedemanipulation/Form1.Designer.cs
+++ Camera_og_billedemanipulation/Form1.Designer.cs
@@ -57,10 +57,10 @@
             // imgVideo
             // 
             this.imgVideo.BackColor = System.Drawing.Color.Gainsboro;
-            this.imgVideo.Location = new System.Drawing.Point(9, 29);
-            this.imgVideo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.imgVideo.Location = new System.Drawing.Point(12, 36);
+            this.imgVideo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.imgVideo.Name = "imgVideo";
-            this.imgVideo.Size = new System.Drawing.Size(325, 202);
+            this.imgVideo.Size = new System.Drawing.Size(433, 249);
             this.imgVideo.TabIndex = 0;
             this.imgVideo.TabStop = false;
             // 
@@ -69,10 +69,10 @@
             this.buttonCapture.BackColor = System.Drawing.Color.Red;
             this.buttonCapture.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonCapture.ForeColor = System.Drawing.Color.White;
-            this.buttonCapture.Location = new System.Drawing.Point(9, 234);
-            this.buttonCapture.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonCapture.Location = new System.Drawing.Point(12, 288);
+            this.buttonCapture.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonCapture.Name = "buttonCapture";
-            this.buttonCapture.Size = new System.Drawing.Size(284, 58);
+            this.buttonCapture.Size = new System.Drawing.Size(379, 71);
             this.buttonCapture.TabIndex = 1;
             this.buttonCapture.Text = "Capture";
             this.buttonCapture.UseVisualStyleBackColor = false;
@@ -81,18 +81,18 @@
             // comboBoxCameraList
             // 
             this.comboBoxCameraList.FormattingEnabled = true;
-            this.comboBoxCameraList.Location = new System.Drawing.Point(338, 2);
-            this.comboBoxCameraList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBoxCameraList.Location = new System.Drawing.Point(451, 2);
+            this.comboBoxCameraList.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxCameraList.Name = "comboBoxCameraList";
-            this.comboBoxCameraList.Size = new System.Drawing.Size(215, 21);
+            this.comboBoxCameraList.Size = new System.Drawing.Size(285, 24);
             this.comboBoxCameraList.TabIndex = 2;
             // 
             // buttonCamStart
             // 
-            this.buttonCamStart.Location = new System.Drawing.Point(230, 2);
-            this.buttonCamStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonCamStart.Location = new System.Drawing.Point(307, 2);
+            this.buttonCamStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonCamStart.Name = "buttonCamStart";
-            this.buttonCamStart.Size = new System.Drawing.Size(50, 20);
+            this.buttonCamStart.Size = new System.Drawing.Size(67, 25);
             this.buttonCamStart.TabIndex = 3;
             this.buttonCamStart.Text = "Start";
             this.buttonCamStart.UseVisualStyleBackColor = true;
@@ -101,10 +101,10 @@
             // imgCapture
             // 
             this.imgCapture.BackColor = System.Drawing.Color.Gainsboro;
-            this.imgCapture.Location = new System.Drawing.Point(9, 297);
-            this.imgCapture.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.imgCapture.Location = new System.Drawing.Point(12, 366);
+            this.imgCapture.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.imgCapture.Name = "imgCapture";
-            this.imgCapture.Size = new System.Drawing.Size(325, 202);
+            this.imgCapture.Size = new System.Drawing.Size(433, 249);
             this.imgCapture.TabIndex = 4;
             this.imgCapture.TabStop = false;
             // 
@@ -113,11 +113,11 @@
             this.buttonGray.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.buttonGray.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonGray.ForeColor = System.Drawing.Color.White;
-            this.buttonGray.Location = new System.Drawing.Point(353, 321);
-            this.buttonGray.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonGray.Location = new System.Drawing.Point(471, 395);
+            this.buttonGray.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonGray.Name = "buttonGray";
             this.buttonGray.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.buttonGray.Size = new System.Drawing.Size(172, 81);
+            this.buttonGray.Size = new System.Drawing.Size(229, 100);
             this.buttonGray.TabIndex = 15;
             this.buttonGray.Text = "Gray";
             this.buttonGray.UseVisualStyleBackColor = false;
@@ -133,45 +133,45 @@
             this.saveToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(563, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(751, 28);
             this.menuStrip1.TabIndex = 9;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // undoToolStripMenuItem
             // 
             this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
-            this.undoToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
             this.undoToolStripMenuItem.Text = "&Undo";
             this.undoToolStripMenuItem.Click += new System.EventHandler(this.undoToolStripMenuItem_Click);
             // 
             // resolutionToolStripMenuItem
             // 
             this.resolutionToolStripMenuItem.Name = "resolutionToolStripMenuItem";
-            this.resolutionToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.resolutionToolStripMenuItem.Size = new System.Drawing.Size(93, 24);
             this.resolutionToolStripMenuItem.Text = "&Resolution";
             this.resolutionToolStripMenuItem.Click += new System.EventHandler(this.resolutionToolStripMenuItem_Click);
             // 
             // loadImageToolStripMenuItem
             // 
             this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
-            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
             this.loadImageToolStripMenuItem.Text = "&Load";
             this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(54, 24);
             this.saveToolStripMenuItem.Text = "&Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // buttonStop
             // 
-            this.buttonStop.Location = new System.Drawing.Point(284, 2);
-            this.buttonStop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonStop.Location = new System.Drawing.Point(379, 2);
+            this.buttonStop.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonStop.Name = "buttonStop";
-            this.buttonStop.Size = new System.Drawing.Size(50, 20);
+            this.buttonStop.Size = new System.Drawing.Size(67, 25);
             this.buttonStop.TabIndex = 11;
             this.buttonStop.Text = "Stop";
             this.buttonStop.UseVisualStyleBackColor = true;
@@ -182,10 +182,10 @@
             this.buttonGreen.BackColor = System.Drawing.Color.Green;
             this.buttonGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonGreen.ForeColor = System.Drawing.Color.White;
-            this.buttonGreen.Location = new System.Drawing.Point(353, 126);
-            this.buttonGreen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonGreen.Location = new System.Drawing.Point(471, 155);
+            this.buttonGreen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonGreen.Name = "buttonGreen";
-            this.buttonGreen.Size = new System.Drawing.Size(172, 81);
+            this.buttonGreen.Size = new System.Drawing.Size(229, 100);
             this.buttonGreen.TabIndex = 13;
             this.buttonGreen.Text = "Green";
             this.buttonGreen.UseVisualStyleBackColor = false;
@@ -196,10 +196,10 @@
             this.buttonRed.BackColor = System.Drawing.Color.Red;
             this.buttonRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonRed.ForeColor = System.Drawing.Color.White;
-            this.buttonRed.Location = new System.Drawing.Point(353, 29);
-            this.buttonRed.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonRed.Location = new System.Drawing.Point(471, 36);
+            this.buttonRed.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonRed.Name = "buttonRed";
-            this.buttonRed.Size = new System.Drawing.Size(172, 81);
+            this.buttonRed.Size = new System.Drawing.Size(229, 100);
             this.buttonRed.TabIndex = 12;
             this.buttonRed.Text = "Red";
             this.buttonRed.UseVisualStyleBackColor = false;
@@ -210,10 +210,10 @@
             this.buttonBlue.BackColor = System.Drawing.Color.Blue;
             this.buttonBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonBlue.ForeColor = System.Drawing.Color.White;
-            this.buttonBlue.Location = new System.Drawing.Point(353, 223);
-            this.buttonBlue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonBlue.Location = new System.Drawing.Point(471, 274);
+            this.buttonBlue.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonBlue.Name = "buttonBlue";
-            this.buttonBlue.Size = new System.Drawing.Size(172, 81);
+            this.buttonBlue.Size = new System.Drawing.Size(229, 100);
             this.buttonBlue.TabIndex = 14;
             this.buttonBlue.Text = "Blue";
             this.buttonBlue.UseVisualStyleBackColor = false;
@@ -221,9 +221,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(434, 29);
+            this.pictureBox1.Location = new System.Drawing.Point(579, 36);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(119, 44);
+            this.pictureBox1.Size = new System.Drawing.Size(159, 54);
             this.pictureBox1.TabIndex = 15;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Visible = false;
@@ -233,11 +234,11 @@
             this.buttonHistogram.BackColor = System.Drawing.Color.DarkGray;
             this.buttonHistogram.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonHistogram.ForeColor = System.Drawing.Color.White;
-            this.buttonHistogram.Location = new System.Drawing.Point(353, 416);
-            this.buttonHistogram.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonHistogram.Location = new System.Drawing.Point(471, 512);
+            this.buttonHistogram.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonHistogram.Name = "buttonHistogram";
             this.buttonHistogram.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.buttonHistogram.Size = new System.Drawing.Size(172, 81);
+            this.buttonHistogram.Size = new System.Drawing.Size(229, 100);
             this.buttonHistogram.TabIndex = 16;
             this.buttonHistogram.Text = "Histogram";
             this.buttonHistogram.UseVisualStyleBackColor = false;
@@ -249,9 +250,10 @@
             // 
             // buttonTimer
             // 
-            this.buttonTimer.Location = new System.Drawing.Point(299, 237);
+            this.buttonTimer.Location = new System.Drawing.Point(399, 292);
+            this.buttonTimer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonTimer.Name = "buttonTimer";
-            this.buttonTimer.Size = new System.Drawing.Size(35, 55);
+            this.buttonTimer.Size = new System.Drawing.Size(47, 68);
             this.buttonTimer.TabIndex = 17;
             this.buttonTimer.Text = "button1";
             this.buttonTimer.UseVisualStyleBackColor = true;
@@ -259,9 +261,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 506);
+            this.ClientSize = new System.Drawing.Size(751, 623);
             this.Controls.Add(this.buttonTimer);
             this.Controls.Add(this.buttonHistogram);
             this.Controls.Add(this.pictureBox1);
@@ -277,7 +279,7 @@
             this.Controls.Add(this.imgVideo);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "WebCam Tool";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
--- Camera_og_billedemanipulation/Form1.cs
+++ Camera_og_billedemanipulation/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using AForge.Imaging.Filters;
 using AForge.Video;
 using AForge.Video.DirectShow;
 
@@ -20,6 +21,7 @@ namespace Camera_og_billedemanipulation
             InitializeComponent();
             buttonCamStart.Enabled = false;
             gv = new GlobalVars(); // Initialize variable 
+            timer1.Interval = 1000;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -404,6 +406,24 @@ namespace Camera_og_billedemanipulation
         {
             imgVideo.BackColor = color;
             imgCapture.BackColor = color;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Bitmap bt = (Bitmap)imgVideo.Image.Clone();
+
+            // It is only possible to make the theshold on a grayscale picture 
+            // create grayscale filter (BT709) 
+            Grayscale filter = new Grayscale(0.2125, 0.7154, 0.0721);
+
+            // apply the filter 
+            Bitmap grayImage = filter.Apply(bt);
+
+            imgCapture.Image = (System.Drawing.Image)grayImage.Clone();
+        }
+        private void buttonTimer_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
         }
 
     }
